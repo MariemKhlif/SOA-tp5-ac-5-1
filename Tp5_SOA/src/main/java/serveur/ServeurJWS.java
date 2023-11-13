6cc9a2da2abef9557751448f0e8c75b914e5f740
mariemk <101462688+MariemKhlif@users.noreply.github.com>
2023-11-13 18:57:05 +0100

    Add files via upload

---
+++ Tp5_SOA/src/main/java/serveur/ServeurJWS.java
@@ -0,0 +1,15 @@
+package serveur;
+
+import jakarta.xml.ws.Endpoint;
+import service.BanqueService;
+public class ServeurJWS {
+    public static void main(String[] args) {
+        // Crée une instance du banque service
+        BanqueService bs = new BanqueService();
+        // URL du service web
+        String url = "http://localhost:8084/";
+        Endpoint.publish(url, bs);
+        // Affichege de l'URL du serveur
+        System.out.println("Server url: " + url);
+    }
+}
